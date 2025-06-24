e545052ebca73743c56219ee5da20d5ae84e0c9a
Ahmadfzlbs <86222949+Ahmadfzlbs@users.noreply.github.com>
2025-06-24 10:13:52 +0700

    fix: error id ruang riung

--- internal/ruangriung/domain/entity.go
+++ internal/ruangriung/domain/entity.go
@@ -2,17 +2,15 @@ package domain
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type RuangRiung struct {
-	ID uuid.UUID `json:"id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	Title        string    `json:"title"`
+	Description  string    `json:"description"`
 	ScheduleTime time.Time `json:"schedule_time"`
-	Location string `json:"location"`
-	PosterPath string `json:"poster_path"`
-	Performers string `json:"performers"`
-	CreatedAt time.Time `json:"created_at"`
+	Location     string    `json:"location"`
+	PosterPath   string    `json:"poster_path"`
+	Performers   string    `json:"performers"`
+	CreatedAt    time.Time `json:"created_at"`
 }
